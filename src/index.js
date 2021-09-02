17633b2aff4656becfc6a3d9973227d8e91254b6
jbrasko <jbrasko0508@gmail.com>
2021-09-02 12:58:30 -0500

    set up express and mongo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+const mongoUri =
+  'mongodb+srv://admin:passwordpassword@cluster0.rgg2b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+})
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance')
+})
 
 app.get('/', (req, res) => {
   res.send('Hi there!')

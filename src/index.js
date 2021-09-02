9c3f5a26959e5b69d0118903abad702c7331eca8
jbrasko <jbrasko0508@gmail.com>
2021-09-02 13:03:25 -0500

    remove mongo options

--- src/index.js
+++ src/index.js
@@ -5,15 +5,12 @@ const app = express()
 
 const mongoUri =
   'mongodb+srv://admin:passwordpassword@cluster0.rgg2b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-mongoose.connect(mongoUri, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-})
+mongoose.connect(mongoUri)
 mongoose.connection.on('connected', () => {
   console.log('Connected to mongo instance')
 })
 mongoose.connection.on('error', err => {
-  console.error('Error connecting to mong', err)
+  console.error('Error connecting to mongo', err)
 })
 
 app.get('/', (req, res) => {

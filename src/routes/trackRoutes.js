de43757ae9f98f9327fcac165fc01d56ea253aaa
jbrasko <jbrasko0508@gmail.com>
2021-09-03 14:42:52 -0500

    build track post route

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -14,4 +14,22 @@ router.get('/tracks', async (req, res) => {
   res.send(tracks)
 })
 
+route.post('/track', async (req, res) => {
+  const { name, locations } = req.body
+
+  if (!name || !locations) {
+    return res
+      .status(422)
+      .send({ error: 'You must provide a name and locations' })
+  }
+
+  try {
+    const track = new Track({ name, locations, userId: req.user._id })
+    await track.save()
+    res.send(track)
+  } catch (err) {
+    res.status(422).send({ error: err.message })
+  }
+})
+
 module.exports = router

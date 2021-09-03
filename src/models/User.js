4f0dd05c04bdb90701d0037f845b7ae410e3ae7d
jbrasko <jbrasko0508@gmail.com>
2021-09-03 13:01:23 -0500

    write comparePassword

--- src/models/User.js
+++ src/models/User.js
@@ -35,4 +35,22 @@ userSchema.pre('save', function (next) {
   })
 })
 
+userSchema.methods.comparePassword = function (candidatePassword) {
+  const user = this
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+      if (err) {
+        return reject(err)
+      }
+
+      if (!isMatch) {
+        return reject(false)
+      }
+
+      resolve(true)
+    })
+  })
+}
+
 mongoose.model('User', userSchema)
